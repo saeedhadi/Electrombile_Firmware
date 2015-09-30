e2d055c207e31f853015d9547c148e1af5cef146
gcy346447065 <gcy346447065@gmail.com>
2015-09-30 17:04:45 +0800

    增加seek value的阈值检测

--- user/electrombile/seek.c
+++ user/electrombile/seek.c
@@ -20,6 +20,10 @@
 
 #define EAT_ADC0 23
 #define EAT_ADC1 24
+
+#define SEEK_INTENSITY_MIN 400
+#define SEEK_INTENSITY_MAX 2000
+
 static void seek_timer_handler(void);
 static eat_bool seek_getValue(int* value);
 static eat_bool seek_sendMsg2Main(MSG_THREAD* msg, u8 len);
@@ -32,7 +36,7 @@ void adc_cb_proc(EatAdc_st* adc)
     if(adc->pin == EAT_ADC0)
     {
         adcdata0 = adc->v;
-        LOG_DEBUG("adcdata0= %f",adcdata0);
+        LOG_DEBUG("adcdata0=%d",adcdata0);
     }
 }
 
@@ -106,11 +110,20 @@ static eat_bool seek_getValue(int* value)
     {
         return EAT_FALSE;
     }
+    else if(SEEK_INTENSITY_MIN > adcdata0)
+    {
+        *value = SEEK_INTENSITY_MIN;
+    }
+    else if(SEEK_INTENSITY_MAX < adcdata0)
+    {
+        *value = SEEK_INTENSITY_MAX;
+    }
     else
     {
         *value = adcdata0;
-        return EAT_TRUE;
     }
+
+    return EAT_TRUE;
 }
 
 static eat_bool seek_sendMsg2Main(MSG_THREAD* msg, u8 len)
