1425f75f7dca495b29a3ff465c58752289af7cbf
Jason <jeson.g@gmail.com>
2015-12-01 17:34:03 +0800

    add LOG_REMOTE

--- user/electrombile/log.h
+++ user/electrombile/log.h
@@ -8,34 +8,36 @@
 #ifndef USER_ELECTROMBILE_LOG_H_
 #define USER_ELECTROMBILE_LOG_H_
 
-#ifdef LOG_DEBUG_SERIAL
+#include <eat_interface.h>
+
+#ifdef LOG_DEBUG_FLAG
 #define LOG_DEBUG(fmt, ...) eat_trace("[DBG][%s:%d %s]"fmt, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
-#elif defined(LOG_DEBUG_REMOTE)
-#define LOG_DEBUG(fmt, ...) log_remote("[%s:%d %s]"fmt,  __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
 #else
 #define LOG_DEBUG(fmt, ...)
 #endif
 
-#ifdef LOG_INFO_SERIAL
+#ifdef LOG_INFO_FLAG
 #define LOG_INFO(fmt, ...) eat_trace("[INF][%s:%d %s]"fmt, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
-#elif defined(LOG_INFO_REMOTE)
-#define LOG_INFO(fmt, ...) log_remote("[%s:%d %s]"fmt,  __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
 #else
 #define LOG_INFO(fmt, ...)
 #endif
 
-#ifdef LOG_ERROR_SERIAL
+#ifdef LOG_ERROR_FLAG
 #define LOG_ERROR(fmt, ...) eat_trace("[ERR][%s:%d %s]"fmt, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
-#elif defined(LOG_ERROR_REMOTE)
-#define LOG_ERROR(fmt, ...) log_remote("[%s:%d %s]"fmt,  __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
 #else
 #define LOG_ERROR(fmt, ...)
 #endif
 
-#ifdef LOG_DEBUG_SERIAL
+#ifdef LOG_DEBUG_FLAG
 #define LOG_HEX(data, len)  log_hex(data, len);
 #else
 #define LOG_HEX(data, len)
+#endif
+
+#ifdef LOG_DEBUG_FLAG
+#define LOG_REMOTE(fmt, ...) log_remote("[%s:%d %s]"fmt, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
+#else
+#define LOG_REMOTE(fmt, ...)
 #endif
 
 void log_hex(const char* data, int length);
