8e863ba32e995c13f87a03f56fd7bff96c43ca65
michael_lee__ <641777437@qq.com>
2016-01-26 10:41:45 +0800

    Revert "Revert "fix little bug , adjust the  event between threads""
    This reverts commit e2c3016b51ddb3ebeeb1e3f67fc3b4b006b39e6e.

--- user/electrombile/main.c
+++ user/electrombile/main.c
@@ -130,8 +130,6 @@ void app_main(void *data)
     eat_timer_start(TIMER_AT_CMD, setting.at_cmd_timer_period);
     eat_timer_start(TIMER_GPS_SEND, setting.gps_send_timer_period);
     eat_timer_start(TIMER_HEARTBEAT,setting.heartbeat_timer_period);
-    eat_timer_start(TIMER_RTC_UPDATE,30*1000);
-
 
     while(EAT_TRUE)
     {
