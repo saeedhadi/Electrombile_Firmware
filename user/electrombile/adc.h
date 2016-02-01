76e71947c5394dae53fde1e5e801f316b018130c
Jason <jeson.g@gmail.com>
2016-02-01 19:45:32 +0800

    adc管脚定义单独出来

---
+++ user/electrombile/adc.h
@@ -0,0 +1,18 @@
+/*
+ * adc.h
+ *
+ *  Created on: 2016年2月1日
+ *      Author: jk
+ */
+
+#ifndef USER_ELECTROMBILE_ADC_H_
+#define USER_ELECTROMBILE_ADC_H_
+
+#define EAT_ADC0 EAT_PIN23_ADC1
+#define EAT_ADC1 EAT_PIN24_ADC2
+
+#define ADC_433 EAT_ADC0
+#define ADC_433_PERIOD (2000)  //unit: ms
+
+
+#endif /* USER_ELECTROMBILE_ADC_H_ */
