7542caf769f8b08249739bccd3f3f795f062ccf4
JiangTao <jeson.g@gmail.com>
2016-03-04 20:23:45 +0800

    增加诊断模块

---
+++ user/main/diagnosis.c
@@ -0,0 +1,82 @@
+/*
+ * diagnosis.c
+ *
+ *  Created on: 2016年3月4日
+ *      Author: jk
+ */
+
+#include <eat_type.h>
+#include <eat_periphery.h>
+#include <eat_network.h>
+
+#include "diagnosis.h"
+#include "adc.h"
+#include "log.h"
+
+
+/*
+ * 检测输入电压范围
+ */
+eat_bool diag_batterCheck()
+{
+    eat_bool rc;
+    u32 voltage;
+
+    rc = eat_get_adc_sync(ADC_VOLTAGE, &voltage);
+    if (!rc)
+    {
+        LOG_ERROR("Get battery voltage failed");
+        return EAT_FALSE;
+    }
+
+    //电池电压介于[36v, 60v]之间
+    //FIXME: 根据分压计算区间
+    if (voltage < 36 || voltage > 60)
+    {
+        LOG_ERROR("battery voltage check failed: %d", voltage);
+        return EAT_FALSE;
+    }
+
+    return EAT_TRUE;
+}
+
+
+/*
+ * 检测GSM的信号强度是否 > 13
+ */
+eat_bool diag_gsmSignalCheck()
+{
+    int csq = eat_network_get_csq();
+    if (csq < 13)
+    {
+        LOG_ERROR("GSM signal quality not enough: %d", csq);
+        return EAT_FALSE;
+    }
+
+    return EAT_TRUE;
+}
+
+/*
+ * 检测433的信号强度
+ */
+eat_bool diag_433Check()
+{
+    eat_bool rc;
+    u32 voltage;
+
+    rc = eat_get_adc_sync(ADC_433, &voltage);
+    if (!rc)
+    {
+        LOG_ERROR("Get 433 signal quality failed");
+        return EAT_FALSE;
+    }
+
+    //检查433信号强度是否在[100mv, 300mv]之间
+    if (voltage < 100 || voltage > 300)
+    {
+        LOG_ERROR("433 signal quality not enough: %d", voltage);
+        return EAT_FALSE;
+    }
+
+    return EAT_TRUE;
+}
---
+++ user/main/diagnosis.h
@@ -0,0 +1,13 @@
+/*
+ * diagnosis.h
+ *
+ *  Created on: 2016年3月4日
+ *      Author: jk
+ */
+
+#ifndef USER_MAIN_DIAGNOSIS_H_
+#define USER_MAIN_DIAGNOSIS_H_
+
+
+
+#endif /* USER_MAIN_DIAGNOSIS_H_ */
