040ad8850077b6cf770a3860387f2d54759a0d5d
JiangTao <jeson.g@gmail.com>
2016-03-04 11:12:16 +0800

    找车模式打开时，才开启AD采样

--- user/main/seek.c
+++ user/main/seek.c
@@ -16,24 +16,16 @@
 
 #define SEEK_AUTO_OFF_PERIOD    30 * 1000   //30 seconds
 
-eat_bool isSeekMode = EAT_FALSE;
-
 void seek_initial(void)
 {
     eat_gpio_setup(EAT_PIN43_GPIO19, EAT_GPIO_DIR_INPUT, EAT_GPIO_LEVEL_LOW);
     eat_gpio_setup(EAT_PIN55_ROW3, EAT_GPIO_DIR_OUTPUT, EAT_GPIO_LEVEL_HIGH);
 
-    eat_adc_get(ADC_433, ADC_433_PERIOD, NULL);
 }
 
 int seek_proc(unsigned int value)
 {
-    if (isSeekMode)
-    {
-        return cmd_seek(value);
-    }
-
-    return 0;
+    return cmd_seek(value);
 }
 
 void seek_startAutoOffTimer(void)
@@ -43,5 +35,12 @@ void seek_startAutoOffTimer(void)
 
 void setSeekMode(eat_bool fixed)
 {
-    isSeekMode = fixed;
+    if (fixed)
+    {
+        eat_adc_get(ADC_433, ADC_433_PERIOD, NULL);
+    }
+    else
+    {
+        eat_adc_get(ADC_433, ADC_PERIOD_READ_ONCE, NULL);
+    }
 }
