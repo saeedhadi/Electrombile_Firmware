2704af31c0b080ab525fac876170cb2033ee0eec
JiangTao <jeson.g@gmail.com>
2016-03-07 13:49:57 +0800

    增加LED模块

---
+++ user/common/led.c
@@ -0,0 +1,27 @@
+/*
+ * led.c
+ *
+ *  Created on: 2016年3月7日
+ *      Author: jk
+ */
+
+
+void LED_on(void)
+{
+
+}
+
+void LED_off(void)
+{
+
+}
+
+void LED_fastBlink(void)
+{
+
+}
+
+void LED_slowBlink(void)
+{
+
+}
