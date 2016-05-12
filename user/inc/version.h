586ac3eb2703508e95452be4ff394a8fd6cbc2b1
jiangtao <jerky168@163.com>
2016-05-12 10:40:01 +0800

    update version number

--- user/inc/version.h
+++ user/inc/version.h
@@ -15,11 +15,12 @@
  * 1.1.1: add diagnosis function
  * 1.2.0: add battery, for RC1
  * 1.2.1: add message re-send mechanism
+ * 1.2.2: add nmealib, and fix the battery bug
  */
 
 #define VERSION_MAJOR   1
 #define VERSION_MINOR   2
-#define VERSION_MICRO   1
+#define VERSION_MICRO   2
 
 #define VERSION_INT(a, b, c)    (a << 16 | b << 8 | c)
 #define VERSION_DOT(a, b, c)    a##.##b##.##c
