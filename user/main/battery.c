c1e5676ec108bacc52992bd98e871cb3c2a2ab68
piggysticker <641777437@qq.com>
2016-08-01 20:32:30 +0800

    Feature/itinerary (#66)
    * 缩小电池类型判定范围，去掉兼容判断
    
    * 车辆行驶过程中不进行电压采集
    
    * 电池类型判断改为趋势判断
    
    * 行驶过程中 不进行电量告警处理  但是进行电池类型判断处理
    
    * 更改变量类型赋值错误  运动过程中不进行报警

--- user/main/battery.c
+++ user/main/battery.c
@@ -109,9 +109,35 @@ static u8 battery_Judge_type(u32 voltage)
     percent = (int)Voltage2Percent(ADvalue_2_Realvalue(voltage));
     percent = percent>MAX_PERCENT_NUM?MAX_PERCENT_NUM:percent;
 
-    if(percent > 30 && percent < 70)
+    if(battery_type == get_battery_type())
     {
-        set_battery_type(battery_type);
+        LOG_DEBUG("battery type OK: %d",battery_type);
+        return (u8)percent;
+    }
+
+    if(60 < percent)
+    {
+        if(battery_type != get_battery_test_type())
+        {
+            LOG_DEBUG("set battery flag: %d",battery_type);
+            set_battery_flag(EAT_TRUE, battery_type);
+        }
+
+        return (u8)percent;
+    }
+
+    if(40 > percent)
+    {
+        if(get_battery_flag() && battery_type == get_battery_test_type())
+        {
+            LOG_DEBUG("set battery type: %d",battery_type);
+            set_battery_type(battery_type);
+        }
+        else
+        {
+            LOG_DEBUG("reset battery type: %d",battery_type);
+            set_battery_flag(EAT_FALSE, NULL);
+        }
     }
 
     return (u8)percent;
@@ -135,7 +161,7 @@ static u8 battery_getType_percent(u32 voltage)
     }
     else if(battery_type == BATTERY_TYPE48)
     {
-        voltage = voltage;
+        voltage = voltage;          //normalizing to 48V
     }
     else if(battery_type == BATTERY_TYPE36)
     {
@@ -143,16 +169,11 @@ static u8 battery_getType_percent(u32 voltage)
     }
     else
     {
-        return (MAX_PERCENT_NUM+1); //BATTERY_TYPENULL as MAX_PERCENT_NUM + 1 > MAX_PERCENT_NUM
+        return (MAX_PERCENT_NUM + 1); //BATTERY_TYPENULL as MAX_PERCENT_NUM + 1 > MAX_PERCENT_NUM
     }
 
     percent = (int)Voltage2Percent(ADvalue_2_Realvalue(voltage));
     percent = percent>MAX_PERCENT_NUM?MAX_PERCENT_NUM:percent;
-
-    if(percent == 0)                //if percent == 0,mostly judged error,set type to default
-    {
-        set_battery_type(BATTERY_TYPENULL);
-    }
 
     return (u8)percent;
 }
@@ -213,9 +234,14 @@ static int battery_alarm_handler(void)
     u8 msgLen = sizeof(MSG_THREAD) + sizeof(ALARM_INFO);
     MSG_THREAD *msg = NULL;
     ALARM_INFO *alarmType = NULL;
-    char type = battery_percent_check();
+    char alarm_type = battery_percent_check();
 
-    if(type == BATTERY_ALARM_NULL)
+    if(Vibration_isMoved())
+    {
+        return 0;
+    }
+
+    if(alarm_type == BATTERY_ALARM_NULL)
     {
         return 0;
     }
@@ -225,9 +251,9 @@ static int battery_alarm_handler(void)
 
     msg->cmd = CMD_THREAD_ALARM;
     msg->length = sizeof(ALARM_INFO);
-    alarmType->alarm_type = type;
+    alarmType->alarm_type = alarm_type;
 
-    LOG_DEBUG("battery alarm:cmd(%d),length(%d),data(%d)", msg->cmd, msg->length, type);
+    LOG_DEBUG("battery alarm:cmd(%d),length(%d),data(%d)", msg->cmd, msg->length, alarm_type);
 
     return sendMsg(THREAD_MAIN, msg, msgLen);
 
@@ -329,10 +355,7 @@ void app_battery_thread(void *data)
                 switch (event.data.timer.timer_id)
                 {
                     case TIMER_BATTERY_CHECK:
-                        if(!Vibration_isMoved())
-                        {
-                            battery_alarm_handler();
-                        }
+                        battery_alarm_handler();
                         eat_timer_start(event.data.timer.timer_id,BATTERY_TIMER_PEROID);
                         break;
 
